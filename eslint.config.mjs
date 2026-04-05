d908f312280672a8e036da84e393d20b27454a14
Mathieu NICOLAS <mathieu.nicolas@me.com>
2026-04-05 03:03:33 +0200

    update

---
+++ eslint.config.mjs
@@ -0,0 +1,10 @@
+import withNuxt from './.nuxt/eslint.config.mjs'
+
+export default withNuxt({
+  rules: {
+    'vue/block-order': ['error', {
+      order: ['script', 'template', 'style'],
+    }],
+
+  }
+})
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+    modules: [
+    '@nuxt/eslint',
+    'nuxt-auth-utils',
+    'nuxt-authorization',
+  ],
+  nitro: {
+    preset: 'cloudflare_module',
+    cloudflare: {
+      deployConfig: true,
+      nodeCompat: true,
+    }
+  },
+  eslint: {
+    config: {
+      stylistic: true,
+    },
+  },
 })
