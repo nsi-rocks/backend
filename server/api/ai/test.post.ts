734d579691527331aadc0d9715c0d51ba880cb55
Mathieu NICOLAS <mathieu.nicolas@me.com>
2026-04-07 11:44:07 +0200

    test IA

---
+++ server/api/ai/test.post.ts
@@ -0,0 +1,25 @@
+export default defineEventHandler(async (event) => {
+    setResponseHeader(event, 'Content-Type', 'text/html')
+    setResponseHeader(event, 'Cache-Control', 'no-cache')
+    setResponseHeader(event, 'Transfer-Encoding', 'chunked')
+
+    const body = await readBody(event)
+    console.log('Body:', body)
+    const config = useRuntimeConfig()
+
+    const response = await $fetch('https://api.mistral.ai/v1/chat/completions', {
+        headers: {
+            'Authorization': `Bearer ${config.mistralApiKey}`,
+            'Content-Type': 'application/json',
+        },
+        method: 'POST',
+        responseType: 'stream',
+        body: JSON.stringify({
+            model: 'mistral-tiny',
+            stream: true,
+            messages: [{ role: 'user', content: body.query }],
+        }),
+    })
+
+    return response
+})
